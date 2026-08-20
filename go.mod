86cdcafc0c9a7c0cc19517fa8beed0eeaf3c412d
Andrej Orlov <orlov.andrej@proton.me>
2026-08-20 13:03:13 +0200

    C1.3

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main
+module github.com/A0RLV/blog
 
 go 1.26.5
--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"strings"
 
 	"github.com/A0RLV/blog/internal/config"
 )
@@ -14,14 +16,35 @@ func main() {
 	}
 	fmt.Printf("Read config: %+v\n", cfg)
 
-	err = cfg.SetUser("lane")
+	s := state{cfg}
+	cs := commands{}
+
+	cs.register("login", config.handlerLogin())
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "Usage: <command> <arguments>")
+		os.Exit(1)
+	}
+	cmd_input := os.Args[1]
+
+	cmd_name := strings.Split(cmd_input, " ")[0]
+	cmd_args := strings.Split(cmd_input, " ")[1:]
+	c := command{cmd_name, cmd_args}
+
+	err := c.config.run(s, c)
 	if err != nil {
-		log.Fatalf("couldn't set current user: %v", err)
+		fmt.Fatalf("%v", err)
 	}
 
-	cfg, err = config.Read()
-	if err != nil {
-		log.Fatalf("error reading config: %v", err)
-	}
-	fmt.Printf("Read config again: %+v\n", cfg)
+	// err = cfg.SetUser("kaz")
+	// if err != nil {
+	// 	log.Fatalf("couldn't set current user: %v", err)
+	// }
+
+	// cfg, err = config.Read()
+	// if err != nil {
+	// 	log.Fatalf("error reading config: %v", err)
+	// }
+	// fmt.Printf("Read config again: %+v\n", cfg)
+
+
 }
