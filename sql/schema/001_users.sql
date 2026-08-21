220a865c289a17232b1344feb683cce640fd8615
Andrej Orlov <orlov.andrej@proton.me>
2026-08-21 07:57:10 +0200

    final

--- sql/schema/001_users.sql
+++ sql/schema/001_users.sql
@@ -1,10 +1,21 @@
--- +goose Up
-CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    created_at TIMESTAMP NOT NULL,
-    updated_at TIMESTAMP NOT NULL,
-    name TEXT NOT NULL UNIQUE
-);
+-- name: CreateUser :one
+INSERT INTO users (id, created_at, updated_at, name)
+VALUES (
+    $1,
+    $2,
+    $3,
+    $4
+)
+RETURNING *;
 
--- +goose Down
-DROP TABLE users;
+-- name: GetUser :one
+SELECT * FROM users WHERE name = $1;
+
+-- name: DeleteUsers :exec
+DELETE FROM users;
+
+-- name: GetUsers :many
+SELECT * FROM users;
+
+-- name: GetUserById :one
+SELECT * FROM users WHERE id = $1;
